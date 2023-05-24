927259c44d88f6e846d58b6dfd60fe302337c4e4
John Ryan <ryjohn@google.com>
2023-05-24 14:40:43 -0700

    Add copyright headers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'dart:convert';
+// Copyright 2023 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:flutter/material.dart';
 import 'package:palm_api_app/api.dart';
--- lib/src/api.g.dart
+++ lib/src/api.g.dart
@@ -1,5 +1,9 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// Copyright 2023 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 part of 'api.dart';
 
 // **************************************************************************
